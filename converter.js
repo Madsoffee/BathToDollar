172b9cc549bc79451844f3e3b14de43b755f784a
Madsoffee Yako <s5935512073@phuket.psu.ac.th>
2019-11-01 20:59:28 +0700

    first commit

---
+++ converter.js
@@ -0,0 +1,7 @@
+function converter(currency) {
+    if(typeof currency === 'number') 
+        return `$${(currency*0.033).toFixed(3)}`
+    else  
+        return "error"
+}
+module.exports = converter;
